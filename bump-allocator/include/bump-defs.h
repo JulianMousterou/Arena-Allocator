617e1560f0c08dd0758e0f10d857e1292e6eaed6
JulianMousterou <julian.mousterou@gmail.com>
2025-09-01 20:37:33 -0400

    Update bump-defs.h

--- bump-allocator/include/bump-defs.h
+++ bump-allocator/include/bump-defs.h
@@ -4,18 +4,19 @@
 #define MEMSIZE 4096
 #define ALIGN_TO_PAGE(x) ((x+15) & ~15)
 typedef struct Metadata {
-	size_t size; 
+        size_t size; 
 } Metadata;
 static void* bump_start = NULL; 
 static void* bump_current = NULL; 
 static void* bump_end = NULL; 
-void* allocBump(size_t size); 
+int is_valid_ptr(void* user_ptr);
 void initBump(void); 
-void freeBump(void* user_ptr); 
+void* allocBump(size_t size); 
+void* callocBump(size_t size);
 void* reallocBump(void* user_ptr, size_t newsize);
+void freeBump(void* user_ptr); 
 void expandBump(size_t newsize);
 void resetBump(void); 
-void* callocBump(size_t size);
 size_t usedBytes(void);
 size_t freeBytes(void); 
 size_t totalBytes(void); 
